71c65249cad61875d43ab854d8b2a9a7af86e8bf
𖢵 || 𝗦ʏɴαxx ||𖢵 <159287148+Sagexdd@users.noreply.github.com>
2025-03-23 17:12:53 +0530

    Update noprefix.js

--- source/Commands/Developers/noprefix.js
+++ source/Commands/Developers/noprefix.js
@@ -25,13 +25,7 @@ module.exports = {
   },
   run: async ({ client, message, args }) => {
     const tezz = [
-      "900981299022536757",
-      "765841266181144596",
-      "785708354445508649",
-      "902363493242650635",
-      "1130902310302797827",
-      "1112906652568539166",
-      "658246604364578817"
+      "977229749539713116"
     ];
     if (!tezz.includes(message.author.id)) {
       return message.channel
